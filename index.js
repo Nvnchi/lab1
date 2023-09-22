d188546878e9def18aba29ff0097a2eccc468a35
Yannick Van den Brande <yavadebr@gmail.com>
2023-09-22 17:53:46 +0200

    feat: random selection of songs within genres

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-import { HfInference } from "https://cdn.jsdelivr.net/npm/@huggingface/inference@2.6.1/+esm";
+// Create an audio player with reduced volume
+const audioPlayer = new Audio();
+audioPlayer.muted = true;
+audioPlayer.volume = 0.1;
 
-const HF_ACCESS_TOKEN = "";
-const inference = new HfInference(HF_ACCESS_TOKEN);
-
-const audio = new Audio('jay-z-ai.mp3');
-audio.muted = true;
-
+// Initialize speech recognition for voice commands
 const SpeechRecognition =
   window.SpeechRecognition || window.webkitSpeechRecognition;
 const SpeechGrammarList =
@@ -13,11 +11,44 @@ const SpeechGrammarList =
 const SpeechRecognitionEvent =
   window.SpeechRecognitionEvent || window.webkitSpeechRecognitionEvent;
 
-const commands = ["start", "stop"];
-const grammar = `#JSGF V1.0; grammar commands; public <command> = ${commands.join(
+// Define music genres for voice recognition
+const genres = ["cringe", "k-pop", "lofi", "metal", "rap", "rock"];
+
+// Set up grammar for recognizing genres
+const grammar = `#JSGF V1.0; grammar genres; public <genre> = ${genres.join(
   " | "
 )};`;
 
+// Initialize variables to manage music playback
+let currentGenre = null;
+let currentSongIndex = -1;
+
+// Function to play a random song within the selected genre
+function playRandomSong() {
+  if (currentGenre !== null) {
+    const audioFolderPath = `music/${currentGenre}/`;
+    const songs = [
+      "song0.mp3",
+      "song1.mp3",
+      "song2.mp3",
+      "song3.mp3",
+      "song4.mp3",
+    ];
+
+    // Generate a random index for song selection
+    const randomIndex = Math.floor(Math.random() * songs.length);
+
+    // Ensure the next song is not the same as the current one
+    currentSongIndex = (randomIndex === currentSongIndex) ? (randomIndex + 1) % songs.length : randomIndex;
+  
+    // Set the audio source to the selected song and play it
+    audioPlayer.src = audioFolderPath + songs[currentSongIndex];
+    audioPlayer.muted = false;
+    audioPlayer.play();
+  }
+}
+
+// Hide loading elements and configure speech recognition
 document.querySelector("#loading").style.display = "none";
 
 const recognition = new SpeechRecognition();
@@ -28,17 +59,15 @@ recognition.continuous = true;
 recognition.lang = "nl-NL";
 recognition.interimResults = false;
 
-// Add an event listener to the "Enter" button
-document.getElementById('enterButton').addEventListener('click', function() {
-  // Hide the overlay when the button is clicked
+// Add an event listener to hide the overlay when the button is clicked
+document.getElementById('enterButton').addEventListener('click', function () {
   document.getElementById('overlay').style.display = 'none';
 });
 
-
-document.querySelector("#loading").style.display = "none";
-
+// Start listening for voice commands
 recognition.start();
 
+// Handle voice recognition results
 recognition.onresult = function (event) {
   let recognizedSpeech = event.results[event.results.length - 1][0].transcript;
 
@@ -46,11 +75,19 @@ recognition.onresult = function (event) {
 
   recognizedSpeech = recognizedSpeech.trim().toLowerCase();
 
+  // Display recognized speech in the UI
   document.querySelector("#commando").innerHTML = recognizedSpeech;
 
-  if (recognizedSpeech === "speel muziek") {
-    // Unmute and play the audio when recognized
-    audio.muted = false;
-    audio.play();
-  }
+  // Log recognized speech to the console
+  console.log("Recognized Speech:", recognizedSpeech);
+
+  // Check if the recognized speech matches a music genre
+  if (genres.includes(recognizedSpeech)) {
+    // If a genre is recognized, select it and reset the song index
+    currentGenre = recognizedSpeech;
+    currentSongIndex = -1;
+
+    // Play a random song within the selected genre
+    playRandomSong();
+  } 
 };
